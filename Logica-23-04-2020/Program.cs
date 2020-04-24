935f024b38c643b18db9eb2a4dc1c96c28734d81
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-04-23 21:55:52 -0300

    Primeiro commit

---
+++ Logica-23-04-2020/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Logica_23_04_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            var nome = Console.ReadLine();
+            Console.WriteLine($"O nome digitado foi {nome}");
+        }
+    }
+}
